b1a604ed6d7b6bfe8f1ece5297eab0ab2c696f8f
Anqi <anqijiang@uchicago.edu>
2024-06-14 04:13:18 -0500

    debugged nan in AxonImaging.remove_common_drift() and AxonImaging.combine_rois()

--- src/axon_preprocessing.py
+++ src/axon_preprocessing.py
@@ -22,6 +22,8 @@ from scipy.stats import lognorm
 from scipy.optimize import curve_fit
 from scipy.stats import weibull_min
 from scipy.signal import savgol_filter
+from ruptures.base import BaseCost
+
 
 # global variables, double check these match your data
 from scipy.sparse import csr_matrix
@@ -35,7 +37,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score, davies_bouldin_score
 from sklearn.metrics import adjusted_mutual_info_score, adjusted_rand_score
 from sklearn.cluster import AgglomerativeClustering
+import ruptures as rpt
 import re
+import numpy.ma as ma
 
 fig_path = 'D:\\Axon\\Methods paper'
 data_path = 'D:\\Axon\\Analysis'
@@ -193,6 +197,32 @@ def f_to_fc3(arr, axis=1, highbound=2, lowbound=0.5):
     return arr
 
 
+# def find_short_intervals(change_points, threshold):
+#     change_points = np.insert(np.array(change_points), 0, 0)
+#     differences = np.diff(change_points)
+#     short_interval_indices = np.where(differences < threshold)[0]
+#     if len(short_interval_indices)>0:
+#         short_intervals = [np.arange(change_points[i], change_points[i + 1]) for i in short_interval_indices]
+#         return np.hstack(short_intervals)
+#     else:
+#         return []
+
+
+def find_short_intervals(change_points, thresh):
+    # Calculate the differences between consecutive elements
+    np_array = np.insert(np.array(change_points), 0, 0)
+    differences = np.diff(np_array)
+
+    # Find the indices where the difference is smaller than the threshold
+    indices = np.where(differences < thresh)[0]
+
+    # Extract the elements that have differences smaller than the threshold
+    # Include both elements in each pair (np_array[i] and np_array[i + 1])
+    pairs = [(np_array[i], np_array[i + 1]) for i in indices]
+
+    return pairs
+
+
 def find_transients(fc3, rois, prominence_thresh=0.1, amplitude=0.12, interval=10, width_min=5, width_max=100):
 
     transients_mat = np.zeros_like(fc3)
@@ -205,7 +235,7 @@ def find_transients(fc3, rois, prominence_thresh=0.1, amplitude=0.12, interval=1
 
     final_rois = np.where(np.sum(transients_mat, axis=1)>0)[0]
 
-    return transients_mat[final_rois], rois[final_rois]
+    return transients_mat[final_rois, :], rois[final_rois]
 
 
 def group_columns(data, n_comp, group_ids, rois):
@@ -228,7 +258,6 @@ def group_columns(data, n_comp, group_ids, rois):
         grouped_components.append(np.squeeze(result))
 
     return np.vstack(grouped_components), group_map
-
 
 class AxonImaging:
 
@@ -297,6 +326,61 @@ class AxonImaging:
 
         return proc_dict
 
+    def remove_common_drift(self, proc_dict=None, cost_fun = None, plot_n = 5, max_drift_frames = 500):
+
+        pca = PCA(n_components=1)
+
+        if cost_fun is None:
+            model = rpt.BottomUp(model='l2')
+        else:
+            model = rpt.BottomUp(custom_cost=cost_fun)
+
+        if proc_dict is None:
+            proc_dict = self._load_dict()
+
+        for env in proc_dict:
+            data = proc_dict[env]['data']
+            rois = proc_dict[env]['roi']
+
+            z_data = scipy.stats.zscore(data[rois, :], axis=1)
+            z1 = pca.fit_transform(z_data.transpose())
+
+            plt.plot(z1)
+            plt.title(f'{self.animal} {self.day} in {env} PCA 1st component')
+            plt.show()
+
+            n_breakpoints = int(input(f"{self.animal} {self.day} in {env} # breakpoints to find"))
+
+            if n_breakpoints>0:
+
+                algo_binseg = model.fit(z1)
+                optimal_change_points = algo_binseg.predict(n_bkps=n_breakpoints)
+                print(optimal_change_points)
+                outlier_indices = find_short_intervals(optimal_change_points, thresh=max_drift_frames)
+                rpt.display(z1, optimal_change_points)
+                #plt.plot(outlier_indices, [0]*len(outlier_indices), color='black', linewidth=3)
+                plt.title(f'{self.animal} {self.day} in {env} {n_breakpoints} breakpoints')
+                plt.savefig(os.path.join(self.path, f'{self.animal}_{self.day}_{env}_ROI_PCA.svg'))
+                plt.show()
+                print(outlier_indices)
+
+                for n in range(plot_n):
+                    rpt.display(z_data[n, :], optimal_change_points)
+                    plt.title(f'cell {rois[n]}: {n_breakpoints} breakpoints')
+                    plt.savefig(os.path.join(self.path, f'{self.animal}_{self.day}_{env}_roi{rois[n]}.svg'))
+                    plt.show()
+
+                verified = int(input(f"are breakpoints reasonable in {self.animal} {self.day} in {env}? 0: no, 1: yes"))
+
+                if verified == 1:
+                    proc_dict[env]['del frames'] = outlier_indices
+                    self._save_dict(proc_dict)
+
+                else:
+                    print(f'user interrupt: {self.animal} {self.day} in {env}')
+
+        return proc_dict
+
     def plot_choose_rois(self, proc_dict=None, worst_n=10):
 
         if proc_dict is None:
@@ -312,36 +396,65 @@ class AxonImaging:
                 for n in range(len(worst_rois)):
                     plt.plot(proc_dict[env]['data'][worst_rois[n], :])
                     plt.title(f'{self.animal} {self.day} in {env} worst {n + plotted_rois} roi: {worst_rois[n]}')
+                    plt.savefig(os.path.join(self.path, f'{self.animal}_{self.day}_{env}_roi{worst_rois[n]}'))
                     plt.show()
 
                 plotted_rois = plotted_rois + n
                 selected_rois = int(input(f"{self.animal} {self.day} in {env} worst # rois to delete"))
 
-            proc_dict[env]['roi'] = proc_dict[env]['roi'][:-selected_rois-1]
+            proc_dict[env]['del roi'] = selected_rois
 
         self._save_dict(proc_dict)
 
         return proc_dict
 
-    def combine_rois(self, proc_dict=None):
+    def combine_rois(self, proc_dict=None, corr_thresh=0.7):
 
         if proc_dict is None:
             proc_dict = self._load_dict()
 
         final_data_list = []
+        deleted_frames = []
+        total_frames = 0
 
         for env, proc_data in proc_dict.items():
             rois = proc_data['roi']
-            data = convert_df_f(proc_data['data'][rois, :])
+
+            if 'del roi' in proc_data:
+                print(f'found rois to delete in {self.animal} {self.day} {self.env}')
+                del_rois = proc_data['del roi']
+                rois = rois[:-del_rois]
+
+            data = proc_data['data'][rois, :]
+
+            if 'del frames' in proc_data:
+                print(f'found frames to delete in {self.animal} {self.day} {self.env}')
+                #del_frames = proc_data['del frames']
+                del_frames = np.hstack([np.arange(n[0], n[1]) for n in proc_data['del frames']])
+                data = np.delete(data, np.unique(del_frames), axis=1)
+                deleted_frames.append(del_frames+total_frames)
+
+            data = convert_df_f(data)
             data = f_to_fc3(data)
             data, selected_rois = find_transients(data, rois)
-            final_mat = np.zeros_like(proc_data['data'])
+            final_mat = np.zeros((proc_data['data'].shape[0], data.shape[1]))
             final_mat[selected_rois, :] = data
             final_data_list.append(final_mat)
+            total_frames = total_frames+proc_data['data'].shape[1]
 
         final_mat = np.hstack(final_data_list)
 
-        combined_mat, group_map = self._group_rois(final_mat)
+        if len(self.env) == 1:
+            combined_mat, group_map = self._group_by_cluster(final_mat, corr_thresh=corr_thresh)
+        elif len(self.env) > 1:
+            combined_mat, group_map = self._group_by_corr(final_mat, corr_thresh=corr_thresh)
+        else:
+            print(f'error {self.animal} {self.day}')
+
+
+        if len(deleted_frames)>0:
+            deleted_frames = np.hstack(deleted_frames)
+            combined_mat = np.insert(combined_mat, deleted_frames-np.arange(len(deleted_frames)), np.nan, axis=1)
 
         save_axon(combined_mat, group_map, self.path)
 
@@ -349,33 +462,36 @@ class AxonImaging:
 
         return combined_mat, group_map, df
 
-    def _group_rois(self, final_mat, corr_thresh=0.7, stopping_thresh_perc=10, min_best_score=0.4):
+    def _group_by_cluster(self, final_mat, corr_thresh=0.7, stopping_thresh_perc=10, min_best_score=0.4):
 
-        cov_nov = np.corrcoef(final_mat)
+        active_rois = np.where(np.sum(final_mat, axis=1) > 0)[0]
+        cov_nov = np.corrcoef(final_mat[active_rois, :])
         np.fill_diagonal(cov_nov, np.nan)
         max_corr = np.nanmax(cov_nov, axis=0)
 
         grouping_rois = np.where(max_corr > corr_thresh)[0]
-        overall_corr = np.corrcoef(final_mat[grouping_rois, :])
-        n_max_cluster = sum(max_corr > corr_thresh)
-        print(f'{self.animal} {self.day} # potential non-singleton ROIs: {n_max_cluster}')
+        singleton_rois = np.where((max_corr>0) & (max_corr<corr_thresh))[0]
+
+        overall_corr = cov_nov[grouping_rois, :]
+        overall_corr = overall_corr[:, grouping_rois]
+        np.fill_diagonal(overall_corr, 1)
+        n_max_cluster = len(grouping_rois)
+        print(f'{self.animal} {self.day} # potential non-singleton ROIs: {n_max_cluster}, '
+              f'# singleton ROIs: {len(singleton_rois)}')
         plt.hist(max_corr)
         plt.title('max corr per ROI')
         plt.show()
 
-        cluster_number_vector = np.arange(np.max((int(len(overall_corr) / 100), 2)), n_max_cluster)
+        cluster_number_vector = np.arange(np.max((int(n_max_cluster / 100), 2)), n_max_cluster)
         silhouette_all = np.zeros((len(cluster_number_vector)))
-        clusters_all = np.zeros((len(cluster_number_vector), sum(max_corr > corr_thresh)))
+        clusters_all = np.zeros((len(cluster_number_vector), n_max_cluster))
         stopping_thresh = int(np.ceil(n_max_cluster * stopping_thresh_perc/100))
-
-        # Flatten the covariance matrices
-        flattened_cov_matrices = overall_corr.reshape(len(overall_corr), -1)
 
         # Scale the flattened covariance matrices
         scaler = StandardScaler()
-        flattened_cov_matrices_scaled = scaler.fit_transform(flattened_cov_matrices)
+        flattened_cov_matrices_scaled = scaler.fit_transform(overall_corr)
 
-        best_k = None
+        best_k = -1
         best_score = -1
         dropping_count = 0
 
@@ -392,7 +508,6 @@ class AxonImaging:
             # Calculate silhouette score
             silhouette_avg = silhouette_score(flattened_cov_matrices_scaled, clusters)
             silhouette_all[i_cluster] = silhouette_avg
-
 
             if silhouette_avg > best_score:
                 best_score = silhouette_avg
@@ -417,14 +532,29 @@ class AxonImaging:
         plt.show()
 
         if best_score < min_best_score:
-            print(f'{self.animal} {self.day} no significant cluster groups found, consider grouping by correlation')
-            return final_mat, {}
+            print(f'{self.animal} {self.day} no significant cluster groups found, start grouping by correlation')
+            n_comp, group_ids = AxonImaging._group_by_corr(final_mat, corr_thresh)
+            return n_comp, group_ids
 
         print(f'{self.animal} {self.day} best K: {best_k}, best Silhouette: {best_score}')
-        grouped_mat, group_map = group_columns(final_mat[grouping_rois, :], best_k, clusters_all[best_k], grouping_rois)
-        combined_mat = np.vstack([grouped_mat, final_mat[~grouping_rois, :]])
+
+        grouped_mat, group_map = group_columns(final_mat[active_rois[grouping_rois], :], best_k, clusters_all[best_k],
+                                               active_rois[grouping_rois])
+        combined_mat = np.vstack([grouped_mat, final_mat[active_rois[singleton_rois], :]])
+        group_map.update(dict(zip(np.arange(best_k, best_k+len(singleton_rois)), active_rois[singleton_rois])))
 
         return combined_mat, group_map  # , grouping_df.sort_values(by='raw roi').sort_values(by='group id')
+
+    @staticmethod
+    def _group_by_corr(final_mat, corr_thresh):
+
+        active_rois = np.where(np.sum(final_mat, axis=1) > 0)[0]
+        cov_nov = np.corrcoef(final_mat[active_rois, :])
+        n_comp, group_ids = connected_components(csr_matrix(cov_nov > corr_thresh), directed=False,
+                                                 return_labels=True)
+        combined_mat, group_map = group_columns(final_mat[active_rois, :], n_comp, group_ids, active_rois)
+
+        return combined_mat, group_map
 
     def _add_behavior(self, imaging_mat):
         # Create DataFrame from imaging_mat
@@ -448,6 +578,7 @@ class AxonImaging:
         # Clean up lap values and save to Parquet file
         df['lap'] = df['lap'].sub(1).where(df['lap'].ne(0))  # Subtract 1 from 'lap' and replace 0 with NaN
         df = df.dropna(subset=['lap'])  # Drop rows with NaN in 'lap' column
+        df = df.loc[:, (df != 0).any(axis=0)]   # drop columns with only zeros
         df.to_parquet(os.path.join(self.path, f'{self.animal}_{self.day}_axon.parquet'), compression='gzip')
 
         print(f'Saved to parquet in folder {self.path}')
